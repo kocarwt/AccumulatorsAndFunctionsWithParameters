c0ab0e3c67fd6ab7fbad52e7227ed0af2fa5bf01
kocarwt <kocawt@rose-hulman.edu>
2017-11-29 14:12:05 -0500

    working on m6

--- src/m6_summing.py
+++ src/m6_summing.py
@@ -4,8 +4,8 @@ in its simplest classic forms:
    SUMMING:       total = total + number
 
 Authors: David Mutchler, Valerie Galluzzi, Mark Hays, Amanda Stouder,
-         their colleagues and PUT_YOUR_NAME_HERE.
-"""  # TODO: 1. PUT YOUR NAME IN THE ABOVE LINE.
+         their colleagues and William Kocar.
+"""  # Done: 1. PUT YOUR NAME IN THE ABOVE LINE.
 
 
 def main():
@@ -29,8 +29,18 @@ def run_test_sum_cosines():
     print('Testing the   sum_cosines   function:')
     print('--------------------------------------------------')
 
+    expected = .13416
+    answer = sum_cosines(3)
+    print('Test 1 expected:', expected)
+    print('       actual:  ', answer)
+
 
 def sum_cosines(n):
+    total = rg.math(cos(n))
+    for k in range(n):
+    total = total + (cos(k))
+    return total
+
     """
     What comes in:  A non-negative integer n.
     What goes out:  The sum of the cosines of the integers
